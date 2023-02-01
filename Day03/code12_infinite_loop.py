18b9388294dec647974ae30376f30db1143d94fc
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-02-01 12:15:52 +0900

    ㅇ

--- Day03/code12_infinite_loop.py
+++ Day03/code12_infinite_loop.py
@@ -1,4 +1,11 @@
 #무한반복 예제
-key = input('숫자를 입력하세요.')
+num = 0
 
-print(f'안녕하세요, {key}님!!!')
+while True:
+    print('메뉴를 선택하세요')
+    print(' 1. 회원입력')
+    print(' 2. 회원검색')
+    print(' 3. 회원수정')
+    print(' 4. 회원삭제')
+    print(' 5. 프로그램 종료')
+    num = input('메뉴번호 입력 > ')
