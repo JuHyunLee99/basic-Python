200d5584dccac21f62d07a953b29dbf7fd1c7f41
JuHyunLee99 <juhyun.lee0829@daum.net>
2023-02-01 11:40:09 +0900

    다시 중간 커밋

--- Day03/code09_googoodan.py
+++ Day03/code09_googoodan.py
@@ -4,7 +4,7 @@ for x in range(2, 10):
     print(f'{x}단')
 
     for y in range(1, 10):
-        print(f'{x} X {y} = {x * y:2d}', end=' / ')
+        print(f'{x} X {y} = {x*y:>2}', end=' ')
 
 # 디버깅 F5
 # 중단점, 변수, 조사식 
